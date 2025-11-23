97ec7453ff306f6ed25a9b70308220b03468d190
zepherym <zepherym@gmail.com>
2025-11-23 12:46:42 +0100

    Starting with the lexer

---
+++ src/lexer.rs
@@ -0,0 +1,4 @@
+pub fn to_token(input: &str) -> String {
+    println!("{}", input);
+    String::from(input)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+mod lexer;
+
+use std::fs;
+
+use clap::Parser;
+// use regex::Regex;
+
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    input_path: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    let input = fs::read_to_string(&cli.input_path).expect("Should be able to read file");
+
+    let _tokens = lexer::to_token(&input);
 }
